a3f7d85d274e206012c39844aaadc0ac37a7474d
Vance Leon <vanceleon44@gmail.com>
2019-10-08 12:53:39 -0400

    customer migrations

--- models/migrations/20191008124350_customers.js
+++ models/migrations/20191008124350_customers.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('users', tbl => {
+        tbl.increments();
+        tbl.string('email');
+        tbl.string('password')
+        tbl.string('name')
+        tbl.text('address')
+        tbl.integer('type')
+        tbl.timestamp('created_at', { useTz: true });
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('customers');
 };
