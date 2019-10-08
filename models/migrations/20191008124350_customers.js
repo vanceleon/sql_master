790e2c000596e123ca7b3e4b1442d7b27e9dba45
Vance Leon <vanceleon44@gmail.com>
2019-10-08 13:22:19 -0400

    edits to migration

--- models/migrations/20191008124350_customers.js
+++ models/migrations/20191008124350_customers.js
@@ -1,14 +1,13 @@
-
 exports.up = function(knex) {
-    return knex.schema.createTable('users', tbl => {
-        tbl.increments();
-        tbl.string('email');
-        tbl.string('password')
-        tbl.string('name')
-        tbl.text('address')
-        tbl.integer('type')
-        tbl.timestamp('created_at', { useTz: true });
-    });
+  return knex.schema.createTable('customers', tbl => {
+    tbl.increments().primary();
+    tbl.string('email').notNullable();
+    tbl.string('password').notNullable();
+    tbl.string('name');
+    tbl.text('address');
+    tbl.integer('type').notNullable();
+    tbl.timestamp('created_at', { useTz: true });
+  });
 };
 
 exports.down = function(knex) {
