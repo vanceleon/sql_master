b31e90cbe89a60da7483ed45470ef866d79f8d70
Vance Leon <vanceleon44@gmail.com>
2019-10-08 12:37:56 -0400

    finished knexfile boiler

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,33 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-        database: process.env.DB_LOCAL,
-        user: process.env.DB_LOCAL_USER,
-        password: process.env.DB_LOCAL_PASSWORD
-    }
+      database: process.env.DB_LOCAL,
+      user: process.env.DB_LOCAL_USER,
+      password: process.env.DB_LOCAL_PASSWORD
+    },
+    migrations: {
+      tableName: 'migrations',
+      directory: './models/migrations'
+    },
+    seeds: {
+        directory: './models/seeds'
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './models/migrations/'
+    },
+    seeds: {
+      directory: './models/seeds/'
+    },
+    useNullAsDefault: true
   }
 };
